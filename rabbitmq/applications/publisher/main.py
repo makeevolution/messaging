231eaa0c455363d88dba9861608f21c8527a535c
aldo sebastian <59067699+makeevolution@users.noreply.github.com>
2024-07-06 21:56:12 +0200

    make publisher

--- rabbitmq/applications/publisher/main.py
+++ rabbitmq/applications/publisher/main.py
@@ -6,10 +6,10 @@ import pika
 app = FastAPI()
 
 class Message(BaseModel):
-    content: str
+    routing_key: str
+    message: str
 
-@app.post("/publish/")
-def publish_message(message: Message):
+def publish_message(routing_key:str, message: Message):
     rabbit_host = os.getenv('RABBIT_HOST', 'localhost')
     rabbit_port = int(os.getenv('RABBIT_PORT', '5672'))
     rabbit_user = os.getenv('RABBIT_USERNAME', 'guest')
@@ -17,22 +17,23 @@ def publish_message(message: Message):
 
     credentials = pika.PlainCredentials(rabbit_user, rabbit_password)
     parameters = pika.ConnectionParameters(host=rabbit_host, port=rabbit_port, credentials=credentials)
-
-    try:
-        connection = pika.BlockingConnection(parameters)
+    connection = pika.BlockingConnection(parameters)
         channel = connection.channel() 
-        # This is like get_or_create of Django. If the queue exists already it won't be created.
-        # Otherwise, it will be created.
-        channel.queue_declare(queue='myQueue', durable=True)
+        
         channel.basic_publish(
             exchange='',
-            routing_key='myQueue',  # this is the queue name to publish to
-            body=message.content,
+            routing_key=routing_key,  # this is the queue name to publish to
+            body=message,
             properties=pika.BasicProperties(
                 delivery_mode=2,  # make message persistent
             )
         )
         connection.close()  # make sure network buffers are flushed and message is delivered to RabbitMQ
+
+@app.post("/publish/")
+async def publish(msg: Message):
+    try:
+        publish_message(msg.routing_key, msg.message)
         return {"status": "Message published successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
